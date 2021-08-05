22949b399eab1b32af6f331bb620b02ebbb94202
seattleowl <32573897+seattleowl@users.noreply.github.com>
2021-08-05 08:17:16 -0700

    tutorial add stuff

--- src/main/java/com/seattleowl/mageowlswizardry/MageOwlsWizardry.java
+++ src/main/java/com/seattleowl/mageowlswizardry/MageOwlsWizardry.java
@@ -18,8 +18,8 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("mageowlwizardry")
-public class MageOwlsWizardry
+@Mod("examplemod")
+public class ExampleMod
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
@@ -61,5 +61,16 @@ public class MageOwlsWizardry
     public void onServerStarting(FMLServerStartingEvent event) {
         // do something when the server starts
         LOGGER.info("HELLO from server starting");
+    }
+
+    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+    // Event bus for receiving Registry Events)
+    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+            // register a new block here
+            LOGGER.info("HELLO from Register Block");
+        }
     }
 }
